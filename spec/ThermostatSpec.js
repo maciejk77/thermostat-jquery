7b7dcf516a1a4639d5700aeabf6badbade30cb4a
Maciej Kurek <maciejk77@gmail.com>
2015-08-31 17:18:55 +0200

    Default settings tests passing min/max temp & power saving on/off

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,13 +1,40 @@
-describe('Thermostat', function(){
+describe('Thermostat default settings', function() {
 
   beforeEach(function() {
     thermostat = new Thermostat();
   });
-
+  
   it('has a default temperature of 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20);
   });
 
+  it('has a Power Save mode switched to ON as default', function() {
+    expect(thermostat.powerSave).toBe(true);
+  });
 
+  it('has a minimum temperature of 10 degrees', function(){
+    expect(thermostat.minTemperature).toEqual(10);
+  });
+
+  it('has a maximum temperature of 25 if Power Saving is ON', function() {
+    expect(thermostat.maxTemperature).toEqual(25);
+  });
+
+  it('has a maximum temperature of 32 if Power Saving is OFF', function() {
+    thermostat.powerSaveSwitch();
+    expect(thermostat.maxTemperature).toEqual(32);
+  });
 
 });
+
+// describe('Thermostat features', function(){
+
+//   beforeEach(function() {
+//     thermostat = new Thermostat();
+//   });
+
+//   it('can increase temperature by one degree', function() {
+//     expect(thermostat.temperature).toEqual(21);
+//   });
+
+// });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,6 +1,23 @@
 function Thermostat() {
   this.defaultTemperature = 20;
   this.temperature = this.defaultTemperature;
-
+  this.powerSave = true;
+  this.minTemperature = 10;
+  this.maxTemperature = this.powerSaveCheck();
 };
 
+Thermostat.prototype.powerSaveSwitch = function() {
+  this.powerSave = !(this.powerSave);
+  this.maxTemperature = this.powerSaveCheck();
+  if(this.temperature > this.maxTemperature) {
+    this.temperature = this.maxTemperature;
+  };
+};
+
+Thermostat.prototype.powerSaveCheck = function() {
+  if(this.powerSave === true) {
+    return 25;
+  } else {
+    return 32;
+  }
+};
