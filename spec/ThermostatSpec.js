e0e41b49542b57511f86843627a4ff9c3af5acf6
Maciej Kurek <maciejk77@gmail.com>
2015-08-31 19:34:58 +0200

    Three tests to complete

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -52,4 +52,49 @@ describe('Thermostat features', function() {
     expect(thermostat.temperature).toEqual(thermostat.defaultTemperature);
   });
 
+  it('can not go below its minimum temperature', function() {
+    // var decreaseTimes = (thermostat.temperature - thermostat.minTemperture + 1);
+    // var count = decreaseTimes;
+    // for(i = 0; i < count; i++) {
+    //   thermostat.decrease();
+    // };
+      
+    thermostat.decrease();
+    thermostat.decrease();
+    thermostat.decrease();
+    thermostat.decrease();
+    thermostat.decrease();
+    thermostat.decrease();
+    thermostat.decrease();
+    thermostat.decrease();
+    thermostat.decrease();
+    thermostat.decrease();
+
+    expect(thermostat.temperature).toEqual(thermostat.minTemperature);
+  });
+
+  it('can not go over its maximum temperature', function() {
+  
+  });
+
+  it('can not go over its maximum temperature when Power Saving is ON', function() {
+      thermostat.increase();
+      thermostat.increase();
+      thermostat.increase();
+      thermostat.increase();
+      thermostat.increase();
+      thermostat.increase();
+      thermostat.increase();
+      thermostat.increase();
+      thermostat.increase();
+      thermostat.increase();
+  });
+
+  it('can not go over its maximum temperature if Power Saving is OFF', function() {
+    thermostat.powerSaveSwitch();
+    var increaseTimes = (thermostat.maximum - thermostat.temperature + 1);
+    var count = increaseTimes;
+    for(i = 0; i < count; i++) {}
+  });
+
 });
